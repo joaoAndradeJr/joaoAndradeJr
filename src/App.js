f1d6015d4cb65b7c34c3ca515a76c2a5f1fff4cf
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-05-28 19:57:41 -0400

    texto pagina home incluido

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
     );
   }
 }
